6dd0f868fed132ca4c97bd677a6c4dc4df770a82
Burch, Eric L. (DOC) <eric.burch@doc1.wa.gov>
2024-03-01 08:08:02 -0800

    add loading orbital to projections page

---
+++ Server/Controllers/AllocationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFinance.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AllocationController : Controller
+    {
+        // GET: AllocationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
