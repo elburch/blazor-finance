307f3b04fd9a6c7c0c93dcf68d4742cd43e24a37
Burch, Eric L. (DOC) <eric.burch@doc1.wa.gov>
2024-08-15 16:39:31 -0700

    add Plotly pie chart to Allocations page

--- Shared/Types/IncomeType.cs
+++ Shared/Types/IncomeType.cs
@@ -20,6 +20,43 @@ namespace BlazorFinance.Shared.Types
         [Display(Name = "Wages")]
         Wages,
         [Display(Name = "Withdrawal")]
-        Withdrawal
+        Withdrawal,
+        [Display(Name = "Asset Sale")]
+        AssetSale,
+        [Display(Name = "Miscellaneous")]
+        Miscellaneous
+    }
+
+    public static class IncomeTypes
+    {
+        public static string ColorMap(this IncomeType income)
+        {
+            switch (income)
+            {
+                case IncomeType.None:
+                    return "#ffffff";
+                case IncomeType.Interest:
+                    return "#1e001e";
+                case IncomeType.Dividend:
+                    return "#320032";
+                case IncomeType.Annuity:
+                    return "#450045";
+                case IncomeType.Pension:
+                    return "#590059";
+                case IncomeType.SocialSecurity:
+                    return "#6c006c";
+                case IncomeType.Wages:
+                    return "#800080";
+                case IncomeType.Withdrawal:
+                    return "#940094";
+                case IncomeType.AssetSale:
+                    return "#a700a7";
+                case IncomeType.Miscellaneous:
+                    return "#bb00bb";
+                default:
+                    return "#ffffff";
+
+            }
+        }
     }
 }
