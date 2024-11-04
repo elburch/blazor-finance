1baed468952c8a8254458818dfd14446cac41606
Burch, Eric L. (DOC) <eric.burch@doc1.wa.gov>
2024-11-04 08:33:26 -0800

    replace some blazorise components with local components and complete button styling changes

---
+++ Shared/Models/NetWorthModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorFinance.Shared.Models
+{
+    public class NetWorthModel
+    {
+        public int TrendDuration { get; set; }
+
+        public decimal AnnualInflation { get; set; }
+    }
+}
