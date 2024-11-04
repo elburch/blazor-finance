274585d7ecce9a98ffe819e7b6a4fb3f9d43d90f
Burch, Eric L. (DOC) <eric.burch@doc1.wa.gov>
2024-11-04 08:35:12 -0800

    add controller for positions summary

--- Server/Controllers/PositionController.cs
+++ Server/Controllers/PositionController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlazorFinance.Server.Repositories;
+using BlazorFinance.Server.Services;
+using BlazorFinance.Shared.Entities;
+using BlazorFinance.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using YahooFinanceApi;
 
 namespace BlazorFinance.Server.Controllers
 {
@@ -6,9 +11,52 @@ namespace BlazorFinance.Server.Controllers
     [Route("[controller]")]
     public class PositionController : Controller
     {
-        public IActionResult Index()
+        private IAssetRepository _repository;
+        private readonly IQuoteService _quoteService;
+        private List<Asset> _assets;
+
+        public PositionController(IAssetRepository repository, IQuoteService quoteService)
         {
-            return View();
+            _repository = repository;
+            _quoteService = quoteService;
+            _assets = new List<Asset>();
+        }
+
+        [HttpGet("read")]
+        public async Task<List<StockModel>> Get()
+        {
+            _assets = await _repository.ReadAssetListAsync(x => x.Symbol.Length > 0);
+
+            IReadOnlyDictionary<string, Security> quotes = await _quoteService.GetQuotes(
+                _assets.Select(x => x.Symbol).ToList()
+            );
+
+            return PrepareModel(quotes);
+        }
+
+        internal List<StockModel> PrepareModel(IReadOnlyDictionary<string, Security> quotes)
+        {
+            List<StockModel> models = new List<StockModel>();
+
+            foreach (var quote in quotes)
+            {
+                double shares = _assets
+                    .Where(x => x.Symbol == quote.Value.Symbol)
+                    .Select(x => x.Quantity)
+                    .FirstOrDefault();
+
+                StockModel model = new StockModel();
+
+                model.Symbol = quote.Value.Symbol;
+                model.Name = quote.Value.LongName;
+                model.Quote = (decimal)quote.Value.RegularMarketPrice;
+                model.Shares = shares;
+                model.MarketValue = (decimal)shares * (decimal)quote.Value.RegularMarketPrice;
+
+                models.Add(model);
+            }
+
+            return models;
         }
     }
 }
