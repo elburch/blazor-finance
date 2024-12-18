b4113b4cd54914779404cbb59c646e2fd38080df
Burch, Eric L. (DOC) <eric.burch@doc1.wa.gov>
2024-12-17 17:01:26 -0800

    update asset market values from landing page

---
+++ Server/Controllers/QuoteController.cs
@@ -0,0 +1,51 @@
+﻿using BlazorFinance.Server.Repositories;
+using BlazorFinance.Server.Services;
+using BlazorFinance.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using YahooFinanceApi;
+
+namespace BlazorFinance.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuoteController : Controller
+    {
+        private IAssetRepository _repository;
+        private readonly IQuoteService _quoteService;
+
+        public QuoteController(IAssetRepository assetRepository, IQuoteService quoteService)
+        {
+            _repository = assetRepository;
+            _quoteService = quoteService;
+        }
+
+        [HttpGet("update")]
+        public async Task<IActionResult> UpdateStaleMarketValuesAsync()
+        {
+            List<Asset> assets = await _repository.ReadAssetListAsync(x => x.Symbol.Length > 0 && x.SnapshotDate.Date < DateTime.Today);
+
+            if (assets == null || assets.Count == 0){
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+
+            IReadOnlyDictionary<string, Security> quotes = await _quoteService.GetQuotes(
+                assets.Select(x => x.Symbol).ToList()
+            );
+
+            foreach (var quote in quotes){
+                Asset asset = assets
+                    .Where(x => x.Symbol == quote.Value.Symbol)
+                    .First();
+
+                if (asset != null){
+                    asset.MarketValue = (decimal)asset.Quantity * (decimal)quote.Value.RegularMarketPrice;
+                    asset.SnapshotDate = DateTime.Now;
+                }
+            }
+
+            return await _repository.UpdateAssetsAsync(assets) == quotes.Count
+                ? StatusCode(StatusCodes.Status200OK)
+                : StatusCode(StatusCodes.Status400BadRequest);
+        }
+    }
+}
