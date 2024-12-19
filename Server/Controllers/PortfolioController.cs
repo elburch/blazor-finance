b5e42704a8787e7f91d07c8478250eb9e1c261bc
Burch, Eric L. (DOC) <eric.burch@doc1.wa.gov>
2024-12-18 16:22:41 -0800

    remove asset market value updates from positions page

--- Server/Controllers/PortfolioController.cs
+++ Server/Controllers/PortfolioController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using BlazorFinance.Server.Services;
-using BlazorFinance.Shared.Models;
-using YahooFinanceApi;
-using OoplesFinance.YahooFinanceAPI.Models;
+//using BlazorFinance.Server.Services;
+//using BlazorFinance.Shared.Models;
+//using YahooFinanceApi;
+//using OoplesFinance.YahooFinanceAPI.Models;
 
 namespace BlazorFinance.Server.Controllers
 {
@@ -10,42 +10,6 @@ namespace BlazorFinance.Server.Controllers
     [Route("[controller]")]
     public class PortfolioController : ControllerBase
     {
-        private readonly ILogger<PortfolioController> _logger;
-        private readonly IQuoteService _quoteService;
 
-        public PortfolioController(ILogger<PortfolioController> logger, IQuoteService portfolioSummaryService)
-        {
-            _logger = logger;
-            _quoteService = portfolioSummaryService;
-        }
-
-        [HttpGet]
-        public async Task<List<StockModel>> Get()
-        {
-            // TO DO: hard-coded - need to get stock symbols from LiteDB service layer
-            List<string> symbols = new List<string>() { "AAPL", "GOOG" };
-
-            IReadOnlyDictionary<string, Security> quotes = await _quoteService.GetQuotes(symbols);
-
-            return PrepareModel(quotes);
-        }
-
-        internal List<StockModel> PrepareModel(IReadOnlyDictionary<string, Security> quotes)
-        {
-            List<StockModel> models = new List<StockModel>();
-
-            foreach (var quote in quotes) {
-                StockModel model = new StockModel();
-
-                model.Symbol = quote.Value.Symbol;
-                model.Name = quote.Value.LongName;
-                //model.Shares - need number of shares from "database" here
-                model.Quote = (decimal)quote.Value.RegularMarketPrice;
-
-                models.Add(model);
-            }
-
-            return models;
-        }
     }
 }
