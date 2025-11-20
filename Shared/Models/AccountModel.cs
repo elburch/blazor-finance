313f61841e9e8920c52449232a16f24d54d0f8c2
Burch, Eric <elburch63@gmail.com>
2025-11-20 10:18:40 -0800

    use account type to distribute portfolio income and expenses

---
+++ Shared/Models/AccountModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorFinance.Shared.Types;
+
+namespace BlazorFinance.Shared.Models
+{
+    public class AccountModel
+    {
+        public int Id { get; set; }
+
+        public AccountType Type { get; set; }
+
+        public double AnnualGrowth { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public decimal Balance { get; set; }
+
+        public decimal MarketValue { get; set; }
+    }
+}
